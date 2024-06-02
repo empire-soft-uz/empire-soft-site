27bf7161c578bdd5166886451d057962b394535e
orihero <mebungler@gmail.com>
2024-06-02 14:14:13 +0500

    pngs changed to webp

--- public/assets/ES_NEW/data.js
+++ public/assets/ES_NEW/data.js
@@ -1,75 +1,71 @@
 const projects = [
-  {
-    name: "FIT.ME",
-    promoBackground: "fitme.png",
-    logo: "FIT.ME.jpg",
-    title: "New generation of fitness",
-    description:
-      "Mobile app and website for finding trainers, nutritions and proper set of exercises",
-    screenshot: "fitme-app.png",
-    type:'mobile'
-  },
-  {
-    name: "Pactive",
-    promoBackground: "pactiveBackground.png",
-    logo: "pactive-logo.png",
-    title: "Get Pactive",
-    description:
-      "Lifestyle app designed to bring together social groups & improve their overall health and wellbeing, while raising funds for good causes",
-    screenshot: "pactive-app.png",
-    videoUrl: "pactive.mp4",
-    iosLink: "https://apps.apple.com/uz/app/pactive/id1441960847",
-    androidLink:
-      "https://play.google.com/store/apps/details?id=com.koicapital.pactive&hl=en_US",
-    websiteLink: "",
-    type:'mobile'
-  },
-  {
-    name: "Ezyorganizer",
-    promoBackground: "ezyback.jpg",
-    logo: "ezy.png",
-    title: "Todo life planner",
-    description:
-      "ORGANIZE YOUR TODOs … NOW WITH VOICE CONTROL",
-    screenshot: "ezy-app.pmg",
-    videoUrl: "",
-    iosLink:
-      "https://apps.apple.com/uz/app/ezyorganizer-life-organizer/id1469144912",
-    androidLink:
-      "https://play.google.com/store/apps/details?id=com.aecd9d0bc1e7.app&hl=ru&gl=US",
-    websiteLink: "https://ezyorganizer.com/",
-    type:'mobile'
-  },
-  {
-    name: "ETZsoft",
-    promoBackground: "etz.jpg",
-    logo: "etz-logo.png",
-    title: "New way of handling retirement",
-    description:
-      "Employees can trade 24/7 in their retirement account through our simple to use mobile app.",
-    screenshot: "etz-video.mp4",
-    videoUrl: "",
-    iosLink:
-      "https://apps.apple.com/us/app/etz/id1643138304",
-    androidLink:
-      "https://play.google.com/store/apps/details?id=com.etzsoft.mobile",
-    websiteLink: "https://ezyorganizer.com/",
-    type:'mobile'
-  },
-  {
-    name: "Weebuild.uz",
-    promoBackground: "weebuild-back.jpg",
-    logo: "weebuild-logo.webp",
-    title: "Home interior calculator",
-    description:
-      "Know your expenses and products list when renovating your apartment",
-    screenshot: "weebuild-app.png",
-    videoUrl: "",
-    iosLink:
-      "",
-    androidLink:
-      "",
-    websiteLink: "https://weebuild.uz/",
-    type:'desktop'
-  },
+    {
+        name: "FIT.ME",
+        promoBackground: "fitme.webp",
+        logo: "FIT.ME.webp",
+        title: "New generation of fitness",
+        description:
+            "Mobile app and website for finding trainers, nutritions and proper set of exercises",
+        screenshot: "fitme-app.webp",
+        type: "mobile",
+    },
+    {
+        name: "Pactive",
+        promoBackground: "pactiveBackground.webp",
+        logo: "pactive-logo.webp",
+        title: "Get Pactive",
+        description:
+            "Lifestyle app designed to bring together social groups & improve their overall health and wellbeing, while raising funds for good causes",
+        screenshot: "pactive-app.webp",
+        videoUrl: "pactive.mp4",
+        iosLink: "https://apps.apple.com/uz/app/pactive/id1441960847",
+        androidLink:
+            "https://play.google.com/store/apps/details?id=com.koicapital.pactive&hl=en_US",
+        websiteLink: "",
+        type: "mobile",
+    },
+    {
+        name: "Ezyorganizer",
+        promoBackground: "ezyback.webp",
+        logo: "ezy.webp",
+        title: "Todo life planner",
+        description: "ORGANIZE YOUR TODOs … NOW WITH VOICE CONTROL",
+        screenshot: "ezy-app.webp",
+        videoUrl: "",
+        iosLink:
+            "https://apps.apple.com/uz/app/ezyorganizer-life-organizer/id1469144912",
+        androidLink:
+            "https://play.google.com/store/apps/details?id=com.aecd9d0bc1e7.app&hl=ru&gl=US",
+        websiteLink: "https://ezyorganizer.com/",
+        type: "mobile",
+    },
+    {
+        name: "ETZsoft",
+        promoBackground: "etz.webp",
+        logo: "etz-logo.svg",
+        title: "New way of handling retirement",
+        description:
+            "Employees can trade 24/7 in their retirement account through our simple to use mobile app.",
+        screenshot: "etz-video.mp4",
+        videoUrl: "",
+        iosLink: "https://apps.apple.com/us/app/etz/id1643138304",
+        androidLink:
+            "https://play.google.com/store/apps/details?id=com.etzsoft.mobile",
+        websiteLink: "https://ezyorganizer.com/",
+        type: "mobile",
+    },
+    {
+        name: "Weebuild.uz",
+        promoBackground: "weebuild-back.webp",
+        logo: "weebuild-logo.webp",
+        title: "Home interior calculator",
+        description:
+            "Know your expenses and products list when renovating your apartment",
+        screenshot: "weebuild-app.webp",
+        videoUrl: "",
+        iosLink: "",
+        androidLink: "",
+        websiteLink: "https://weebuild.uz/",
+        type: "desktop",
+    },
 ];
--- public/data.ts
+++ public/data.ts
@@ -1,75 +1,75 @@
 export const projects = [
-  {
-    name: "FIT.ME",
-    promoBackground: "fitme.png",
-    logo: "FIT.ME.jpg",
-    title: "New generation of fitness",
-    description:
-      "Mobile app and website for finding trainers, nutritions and proper set of exercises",
-    screenshot: "Phone-fitme.png",
-    videoUrl: "",
-    iosLink: "",
-    androidLink: "",
-    websiteLink: "",
-    type: "mobile",
-  },
-  {
-    name: "Pactive",
-    promoBackground: "pactiveBackground.png",
-    logo: "pactive-logo.png",
-    title: "Get Pactive",
-    description:
-      "Lifestyle app designed to bring together social groups & improve their overall health and wellbeing, while raising funds for good causes",
-    screenshot: "Phone-pact.png",
-    videoUrl: "pactive.mp4",
-    iosLink: "https://apps.apple.com/uz/app/pactive/id1441960847",
-    androidLink:
-      "https://play.google.com/store/apps/details?id=com.koicapital.pactive&hl=en_US",
-    websiteLink: "",
-    type: "mobile",
-  },
-  {
-    name: "Ezyorganizer",
-    promoBackground: "ezyback.jpg",
-    logo: "ezy.png",
-    title: "Todo life planner",
-    description: "ORGANIZE YOUR TODOs … NOW WITH VOICE CONTROL",
-    screenshot: "Phone-ezy.png",
-    videoUrl: "",
-    iosLink:
-      "https://apps.apple.com/uz/app/ezyorganizer-life-organizer/id1469144912",
-    androidLink:
-      "https://play.google.com/store/apps/details?id=com.aecd9d0bc1e7.app&hl=ru&gl=US",
-    websiteLink: "https://ezyorganizer.com/",
-    type: "mobile",
-  },
-  {
-    name: "ETZsoft",
-    promoBackground: "etz.jpg",
-    logo: "etz-logo.svg",
-    title: "New way of handling retirement",
-    description:
-      "Employees can trade 24/7 in their retirement account through our simple to use mobile app.",
-    screenshot: "Phone-etz.png",
-    videoUrl: "",
-    iosLink: "https://apps.apple.com/us/app/etz/id1643138304",
-    androidLink:
-      "https://play.google.com/store/apps/details?id=com.etzsoft.mobile",
-    websiteLink: "https://ezyorganizer.com/",
-    type: "mobile",
-  },
-  {
-    name: "Weebuild.uz",
-    promoBackground: "weebuild-back.jpg",
-    logo: "weebuild-logo.webp",
-    title: "Home interior calculator",
-    description:
-      "Know your expenses and products list when renovating your apartment",
-    screenshot: "Macbook.png",
-    videoUrl: "",
-    iosLink: "",
-    androidLink: "",
-    websiteLink: "https://weebuild.uz/",
-    type: "desktop",
-  },
+    {
+        name: "FIT.ME",
+        promoBackground: "fitme.webp",
+        logo: "FIT.ME.webp",
+        title: "New generation of fitness",
+        description:
+            "Mobile app and website for finding trainers, nutritions and proper set of exercises",
+        screenshot: "Phone-fitme.webp",
+        videoUrl: "",
+        iosLink: "",
+        androidLink: "",
+        websiteLink: "",
+        type: "mobile",
+    },
+    {
+        name: "Pactive",
+        promoBackground: "pactiveBackground.webp",
+        logo: "pactive-logo.webp",
+        title: "Get Pactive",
+        description:
+            "Lifestyle app designed to bring together social groups & improve their overall health and wellbeing, while raising funds for good causes",
+        screenshot: "Phone-pact.webp",
+        videoUrl: "pactive.mp4",
+        iosLink: "https://apps.apple.com/uz/app/pactive/id1441960847",
+        androidLink:
+            "https://play.google.com/store/apps/details?id=com.koicapital.pactive&hl=en_US",
+        websiteLink: "",
+        type: "mobile",
+    },
+    {
+        name: "Ezyorganizer",
+        promoBackground: "ezyback.webp",
+        logo: "ezy.webp",
+        title: "Todo life planner",
+        description: "ORGANIZE YOUR TODOs … NOW WITH VOICE CONTROL",
+        screenshot: "Phone-ezy.webp",
+        videoUrl: "",
+        iosLink:
+            "https://apps.apple.com/uz/app/ezyorganizer-life-organizer/id1469144912",
+        androidLink:
+            "https://play.google.com/store/apps/details?id=com.aecd9d0bc1e7.app&hl=ru&gl=US",
+        websiteLink: "https://ezyorganizer.com/",
+        type: "mobile",
+    },
+    {
+        name: "ETZsoft",
+        promoBackground: "etz.webp",
+        logo: "etz-logo.svg",
+        title: "New way of handling retirement",
+        description:
+            "Employees can trade 24/7 in their retirement account through our simple to use mobile app.",
+        screenshot: "Phone-etz.webp",
+        videoUrl: "",
+        iosLink: "https://apps.apple.com/us/app/etz/id1643138304",
+        androidLink:
+            "https://play.google.com/store/apps/details?id=com.etzsoft.mobile",
+        websiteLink: "https://ezyorganizer.com/",
+        type: "mobile",
+    },
+    {
+        name: "Weebuild.uz",
+        promoBackground: "weebuild-back.webp",
+        logo: "weebuild-logo.webp",
+        title: "Home interior calculator",
+        description:
+            "Know your expenses and products list when renovating your apartment",
+        screenshot: "Macbook.webp",
+        videoUrl: "",
+        iosLink: "",
+        androidLink: "",
+        websiteLink: "https://weebuild.uz/",
+        type: "desktop",
+    },
 ];
